86bf0604b371b7683783bad188fff6019ca021f2
Yufei Jin <philking492@gmail.com>
2018-12-05 17:51:25 -0800

    Create hangman_client.c

---
+++ hangman_client.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <ctype.h> //tolower()function
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    char msg[256];//client message header,MSG length,Data
+    char r_msg[256];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if(connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr))<0) 
+        error("ERROR connecting");
+    printf("Ready to start game? (y/n):");
+    char temp[256];
+    bzero(temp,256);
+    fgets(temp,10,stdin);
+    if(temp[0]=='n')
+    {
+	    close(sockfd);
+	    return 0;
+    }
+    else if(temp[0]=='y')
+    {
+	printf("breakpoint00");
+	bzero(msg,256);
+	msg[0] = '0';
+	n = write(sockfd,msg,strlen(msg));//game start, send 0 to server
+    	if (n < 0) 
+		error("ERROR writing to socket");
+    	bzero(r_msg,256);
+    	n = read(sockfd,r_msg,255);
+    	if (n < 0) 
+		error("ERROR reading from socket");
+	bzero(temp,256);
+	for(int i=0;i<r_msg[0];i++)
+	{
+		temp[i]=r_msg[i+1];
+	}
+	printf("%s",temp);
+	int end_game = 0;
+	while(end_game)
+	{
+		printf("Letter to guess: ");
+		bzero(msg,256);
+		char msg_temp[256];
+		bzero(msg_temp,256);
+		fgets(msg_temp,255,stdin);
+		int letter = tolower(msg_temp[0]);
+		while(strlen(msg_temp)!=1||letter<97||letter>122)
+		{
+			printf("Error! Please guess one letter.");
+			bzero(msg_temp,256);
+			fgets(msg_temp,255,stdin);
+			letter = tolower(msg_temp[0]);
+		}
+		msg[0] = 1;
+		msg[1] = msg_temp[0];
+		n = write(sockfd,msg,strlen(msg));
+		bzero(r_msg,256);
+		n = read(sockfd,r_msg,255);
+		if(r_msg[0]!=0)
+		{
+			bzero(temp,256);
+			for(int i=0;i<r_msg[0];i++)
+			{
+				temp[i]=r_msg[i+1];
+			}
+			printf("%s",temp);
+			end_game = 1;
+		}
+		else
+		{
+			int word_len = r_msg[1];
+			int inc_len = r_msg[2];
+			bzero(temp,256);
+			for(int i=0;i<word_len;i++)
+			{
+				temp[i] = r_msg[3+i];
+			}
+			printf("%s\n",temp);
+			bzero(temp,256);
+			for(int i=0;i<inc_len;i++)
+			{
+				temp[i] = r_msg[3+word_len];
+			}
+			printf("Incorrect Guesses: %s",temp);
+		}
+	}
+    }
+    close(sockfd);
+    return 0;
+}
