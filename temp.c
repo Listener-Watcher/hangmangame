76a5c64da38c7ffdb9245805aa924e690aa77eb3
Yufei Jin <philking492@gmail.com>
2018-12-06 17:16:24 -0800

    Create temp.c

---
+++ temp.c
@@ -0,0 +1,19 @@
+//Check if it was for closing , and also read the incoming message
+if ((valread = read( sd , buffer, 1024)) == 0)
+                {
+                    //Somebody disconnected , get his details and print
+                    getpeername(sd , (struct sockaddr*)&address , (socklen_t*)&addrlen);
+                    printf("Host disconnected , ip %s , port %d \n" , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+                      
+                    //Close the socket and mark as 0 in list for reuse
+                    close( sd );
+                    client_socket[i] = 0;
+                }
+                  
+                //Echo back the message that came in
+                else
+                {
+                    //set the string terminating NULL byte on the end of the data read
+                    buffer[valread] = '\0';
+                    send(sd , buffer , strlen(buffer) , 0 );
+                }
